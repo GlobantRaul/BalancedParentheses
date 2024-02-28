ab149a00873413add9720a533f038d81bc71f368
GlobantRaul <161527910+GlobantRaul@users.noreply.github.com>
2024-02-28 11:58:21 -0600

    Add files via upload

---
+++ BalancedParentheses.swift
@@ -0,0 +1,30 @@
+﻿func areParenthesesBalanced(_ inputString: String) -> Bool {
+        // If lenght of the input is less than 2, then, the input can not have a balanced parentheses.
+        if inputString.count > 1 {
+                var stringStack: [Character] = []
+                let beginParentheses: Character = "("
+                let closeParentheses: Character = ")"
+        
+                for char in inputString {
+                        // The open parentheses has to be found before the close parentheses.
+                        if char == beginParentheses {
+                                stringStack.append(char)
+                        } else if char == closeParentheses {
+                                if let last = stringStack.last, last == beginParentheses {
+                                        stringStack.removeLast()
+                                } else {
+                                        return false
+                                }
+                        }
+                }
+                return stringStack.isEmpty
+        } else {
+                return false
+        }
+}
+
+let input: String = "())(()"
+let isValid: String = "Valid"
+let isNotValid: String = "Invalid"
+let output = areParenthesesBalanced(input) ? isValid : isNotValid
+print("Output: \(output)")
